f064fc3b353693584ac8d9e88eaaadab90e840b6
Sobchenyuk <ablehands@ukr.net>
2016-06-05 11:10:20 +0300

    05.06.2016

---
+++ zadachnik/src/main/java/zadanie_1/zadanie_1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie_1;
+
+/**
+ *
+ * @author andrey
+ */
+public class zadanie_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world !!!");
+    }
+}
