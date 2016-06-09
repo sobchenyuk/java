8747d3274c75ff7b4ecbfef259715ba8d520d594
Sobchenyuk <ablehands@ukr.net>
2016-06-09 22:00:52 +0300

    09.06.2016(01)

--- zadachnik/src/test/java/hometasks/task1/subtask1/App.java
+++ zadachnik/src/test/java/hometasks/task1/subtask1/App.java
@@ -6,9 +6,42 @@
 package hometasks.task1.subtask1;
 
 /**
- *
+ *С помощью арифметических операций, операций сравнения и конструкций 
+if­else и циклических операций создать приложение которое: 
+● в последовательности чисел от 0 до 100 выводит все значения кратные 2, 3, 5, 
+7 
  * @author andrey
  */
 public class App {
     
+    public static void main(String[] args) {
+  System.out.print("\n"+" значения кратные 2:  ");
+    for (int i = 0; i <= 100; i++) {
+        if (i % 2 ==0){
+  System.out.print(i  + " ");
+    }
+        
+   } 
+  System.out.print("\n"+" значения кратные 3:  ");
+    for (int i = 0; i <= 100; i++) {
+        if (i % 3 ==0){
+  System.out.print(i  + " ");
+    }
+        
+   } 
+    System.out.print("\n"+" значения кратные 5:  ");
+    for (int i = 0; i <= 100; i++) {
+        if (i % 5 ==0){
+  System.out.print(i  + " ");
+    }
+       
+   } 
+    System.out.print("\n"+" значения кратные 7:  ");
+    for (int i = 0; i <= 100; i++) {
+        if (i % 7 ==0){
+  System.out.print(i  + " ");
+    }
+
+   } 
+  }  
 }
